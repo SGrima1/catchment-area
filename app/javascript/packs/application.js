f8c842e0549cb4148e145b9351e93fcd129af2bf
Sebastien Grima <sebastien.grima@gmail.com>
2021-01-13 14:09:20 +0000

    added new location

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -28,9 +28,11 @@ import "bootstrap";
 // Internal imports, e.g:
 // import { initSelect2 } from '../components/init_select2';
 import { initTargomo } from '../plugin/init_targomo'
+import { initAutocomplete } from '../plugin/init_autocomplete'
 
 document.addEventListener('turbolinks:load', () => {
   // Call your functions here, e.g:
   // initSelect2();
   initTargomo();
+  initAutocomplete();
 });
