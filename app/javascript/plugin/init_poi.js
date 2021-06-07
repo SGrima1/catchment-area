503919f204947ea232a27dd164c5772ab3fd8762
Sebastien Grima <sebastien.grima@gmail.com>
2021-06-07 18:58:31 +0100

    returns uniq array

--- app/javascript/plugin/init_poi.js
+++ app/javascript/plugin/init_poi.js
@@ -22,55 +22,32 @@ const initPois = async () => {
   { id: 0, lat: lnglat[1], lng: lnglat[0] }, options
   );
   
-  // remove duplicates function
-
-  
-  // retrieve all asociated postcodes
-//   async function getFromAPI() {
-//     let outcodes = []
-//     pois.features.forEach((poi) => { 
-//       let url = `https://api.postcodes.io/outcodes?lon=${poi.geometry.coordinates[0]}&lat=${poi.geometry.coordinates[1]}&limit=1`
-//       fetch(url)
-//         .then(response => response.json())
-//         .then((data) => {    
-//           outcodes.push(data.result[0].outcode);
-          
-//       });
-
-//     });
-//     return outcodes;
-//   }
-  
-//   async function removeOutcodeDuplicate() {
-//     const outcodesDup = await getFromAPI();
-//     return outcodesDup
-//   }
-
-//   const outcodesGlobal = await removeOutcodeDuplicate();
-//   console.log(outcodesGlobal)
-// };
-
-const asynchronousFunction = async (url) => {
+  // retrieve Outcodes
+  // fetch data from url
+  const asynchronousFunction = async (url) => {
   const response = await fetch(url)
   const resultObject = await response.json()
   return resultObject.result[0].outcode
-}
-const outcodes = []
-const mainFunction = async () => {
-for (const poi of pois.features ){
-  let url = `https://api.postcodes.io/outcodes?lon=${poi.geometry.coordinates[0]}&lat=${poi.geometry.coordinates[1]}&limit=1`
-  
-    const result = await asynchronousFunction(url)
-    outcodes.push(result)
   }
-  return outcodes
-}
-const returnArray = async () => {
-  console.log(await mainFunction())
-}
-
-
-returnArray()
+  
+  // loop through postcodes.io urls and push them into array
+  const outcodes = []
+  const mainFunction = async () => {
+  for (const poi of pois.features ){
+    let url = `https://api.postcodes.io/outcodes?lon=${poi.geometry.coordinates[0]}&lat=${poi.geometry.coordinates[1]}&limit=1`
+    
+      const result = await asynchronousFunction(url)
+      outcodes.push(result)
+    }
+    return outcodes
+  }
+  // return array and manipulate it - return manipulation
+  const returnArray = async () => {
+    const outcodesDup = await mainFunction();
+    const uniq = [...new Set(outcodesDup)];
+    console.log(uniq)
+  }
+  returnArray()
   
 
 }
